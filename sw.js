e3679d5be31f4529599031dcc03264b9aed60449
iwamotoco-source <iwamoto.co@ir-inc.net>
2026-05-15 17:23:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,5 @@
-const CACHE='pwa-v5';
-const ASSETS=['/calendar20777789/','/calendar20777789/index.html','/calendar20777789/manifest.json'];
-
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch',e=>{
-  if(!e.request.url.startsWith(self.location.origin))return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const resClone = res.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, resClone));
-        return res;
-      })
-      .catch(() => caches.match(e.request).then(r=>r||caches.match('/calendar20777789/index.html')))
-  );
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.registration.unregister();
 });
