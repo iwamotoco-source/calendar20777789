44dbed0698bbe5a59e5569f8c5c0a47664173b84
iwamotoco-source <iwamoto.co@ir-inc.net>
2026-05-15 11:27:41 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
-const CACHE='pwa-v2';
-const ASSETS=['/karen20263/','/karen20263/index.html','/karen20263/manifest.json'];
+const CACHE='pwa-v3';
+const ASSETS=['/calendar20777789/','/calendar20777789/index.html','/calendar20777789/manifest.json'];
+
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
   self.skipWaiting();
 });
+
 self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
+
 self.addEventListener('fetch',e=>{
   if(!e.request.url.startsWith(self.location.origin))return;
   e.respondWith(
@@ -17,6 +20,6 @@ self.addEventListener('fetch',e=>{
         caches.open(CACHE).then(cache => cache.put(e.request, resClone));
         return res;
       })
-      .catch(() => caches.match(e.request).then(r=>r||caches.match('/karen20263/index.html')))
+      .catch(() => caches.match(e.request).then(r=>r||caches.match('/calendar20777789/index.html')))
   );
 });
